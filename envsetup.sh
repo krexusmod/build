081f7cab3b4cbf201c0245db8faec9bef4ea133d
Matt Mower <mowerm@gmail.com>
2013-12-29 12:57:20 -0600

    envsetup: repolastsync
    Prints date and time of last repo sync. Example:
    Last repo sync: 11 Jun 2015, 21:54:59 CDT / 12 Jun 2015, 02:54:59 UTC
    
    Change-Id: I4280b500e58237479194ad2e230ed7466db87755
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -20,7 +20,9 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep: Greps on all local sepolicy files.
 - sgrep:   Greps on all local source files.
 - godir:   Go to the directory containing a file.
+- mka:     Builds using SCHED_BATCH on all processors
 - reposync: Parallel repo sync using ionice and SCHED_BATCH
+- repolastsync: Prints date and time of last repo sync.
 
 Environemnt options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -1425,6 +1427,13 @@ function reposync() {
             schedtool -B -n 1 -e ionice -n 1 `which repo` sync -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
             ;;
     esac
+}
+
+function repolastsync() {
+    RLSPATH="$ANDROID_BUILD_TOP/.repo/.repo_fetchtimes.json"
+    RLSLOCAL=$(date -d "$(stat -c %z $RLSPATH)" +"%e %b %Y, %T %Z")
+    RLSUTC=$(date -d "$(stat -c %z $RLSPATH)" -u +"%e %b %Y, %T %Z")
+    echo "Last repo sync: $RLSLOCAL / $RLSUTC"
 }
 
 # Force JAVA_HOME to point to java 1.7 or java 1.6  if it isn't already set.
