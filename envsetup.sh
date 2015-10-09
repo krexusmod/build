dcfad67e03c7634d7aa8807b470b5e8e3ea09aac
George G <kreach3r@users.noreply.github.com>
2015-10-09 06:01:57 +0300

    Export $PRODUCT_DEVICE variable
    After lunch command. This can be used in vendor configs

--- envsetup.sh
+++ envsetup.sh
@@ -532,6 +532,8 @@ function lunch()
         product=
     fi
 
+    local device=$(echo -n $selection | sed -e "s/[^_]*_\([^-]*\).*/\1/")
+
     local variant=$(echo -n $selection | sed -e "s/^[^\-]*-//")
     check_variant $variant
     if [ $? -ne 0 ]
@@ -549,6 +551,7 @@ function lunch()
     fi
 
     export TARGET_PRODUCT=$product
+    export TARGET_DEVICE=$device
     export TARGET_BUILD_VARIANT=$variant
     export TARGET_BUILD_TYPE=release
 
