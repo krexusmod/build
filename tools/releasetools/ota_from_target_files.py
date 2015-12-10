6915e5e2e2829c9f67cdb6cabc6b6967fe76afbe
AndroidRul3z <https://github.com/AndroidRul3z>
2015-12-10 15:20:22 +0200

    Backuptool Support (2/2) (Squashed)
    Squashed commit about backuptool, which merges in all the necessary changes for the tool to work. Requires commit on vendor_icarus.
    
    It squashes the following commits:
    
    build: edify: use set_metadata for backuptool
    
    Also use saner permissions on backuptool.sh
    
    Change-Id: I50742b51867aa358f5924b8dc208833092a35bd9
    
    ota: Include copies of the recovery scripts even if shipping in block mode
    
    ota: Fix recovery-script cleanups in block mode
    
    Change-Id: Iadb480f8062cfb7a0e6c9024e32ac0d71a7481d7
    
    ota_from_target_files: Remove recovery patch condition for block ota
    
    We don't include recovery patch to avoid overwriting custom recoveries.
    Removing this condition allows us to ship block OTAs without the recovery patch.
    
    Change-Id: I48e06b8aaba627d27f2067a3b83a318ac5e96025
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    
    releasetools: clean up residual targetfiles-* dirs in /tmp/
    
    Change-Id: I3792307663f8a85513e102c8bf3d78cf4e514760
    
    releasetools: fix cleaning up /tmp/
    
    * previous commit did exactly nothing...
    
    Change-Id: I66876aff83528596d628d24e60b5d4c0d81577bd

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -134,6 +134,7 @@ OPTIONS.full_bootloader = False
 # Stash size cannot exceed cache_size * threshold.
 OPTIONS.cache_size = None
 OPTIONS.stash_threshold = 0.8
+OPTIONS.backuptool = True
 
 def MostPopularKey(d, default):
   """Given a dict, return the key corresponding to the largest
@@ -531,7 +532,7 @@ def WriteFullOTAPackage(input_zip, output_zip):
       info_dict=OPTIONS.info_dict)
 
   has_recovery_patch = HasRecoveryPatch(input_zip)
-  block_based = OPTIONS.block_based and has_recovery_patch
+  block_based = OPTIONS.block_based
 
   AppendAssertions(script, OPTIONS.info_dict, oem_dict)
   device_specific.FullOTA_Assertions()
@@ -582,6 +583,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
+
+  if OPTIONS.backuptool:
+    if block_based:
+      common.ZipWriteStr(output_zip, "system/bin/backuptool.sh",
+                     ""+input_zip.read("SYSTEM/bin/backuptool.sh"))
+      common.ZipWriteStr(output_zip, "system/bin/backuptool.functions",
+                     ""+input_zip.read("SYSTEM/bin/backuptool.functions"))
+    script.Mount("/system")
+    script.RunBackup("backup")
+    script.Unmount("/system")
 
   system_progress = 0.75
 
@@ -651,6 +662,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
+
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.02, 10)
+    if block_based:
+      script.Mount("/system")
+    script.RunBackup("restore")
+    if block_based:
+      script.Unmount("/system")
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
