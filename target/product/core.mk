b87c1bd093eceb26367764262915f06f3a76d45a
George G <kreach3r@users.noreply.github.com>
2016-04-01 01:47:36 +0300

    Drop AOSP Calculator & DeskClock (1/2)
    Google has released both of them in the PlayStore. The time has come.

--- target/product/core.mk
+++ target/product/core.mk
@@ -25,10 +25,8 @@ PRODUCT_PACKAGES += \
     CaptivePortalLogin \
     CertInstaller \
     Contacts \
-    DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
